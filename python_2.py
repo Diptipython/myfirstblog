37dd6b838524db20f62ef8b1f716ff21ca0f639a
Dipti <diptiad1@gmail.com>
2019-04-09 14:47:14 +0200

    My Django Girls app, first commit

---
+++ python_2.py
@@ -0,0 +1,9 @@
+volume = 80
+if volume < 20:
+    print("It's quiet")
+elif volume >= 20 and volume < 50:
+    print("It's medium")
+elif 50 <= volume <= 80:
+    print("Not Good")
+elif volume > 80:
+    print("Too loud")
