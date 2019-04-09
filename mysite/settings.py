2bf225986621914a06d0c9650bc3bdfe6e598ed0
Dipti <diptiad1@gmail.com>
2019-04-09 16:04:01 +0200

    Edit Allowed host in setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x4o6+a+kw#9jgz)ndc(rc@nd&6=w=)w-x$ixg_ws+nwr_wv&f-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
